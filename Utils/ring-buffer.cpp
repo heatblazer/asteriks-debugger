bc046677a3daa37104f51ee67bc445615b97abfe
heatblazer <ilianzapryanov@abv.bg>
2016-09-13 22:29:01 +0300

    Utils dir added: ringbuffer for sound streams

---
+++ Utils/ring-buffer.cpp
@@ -0,0 +1,6 @@
+#include "ring-buffer.h"
+
+namespace izdebug {
+
+} // namespace izdebug
+
--- Utils/ring-buffer.h
+++ Utils/ring-buffer.h
@@ -1,4 +1,23 @@
 #ifndef RINGBUFFER_H
 #define RINGBUFFER_H
 
+namespace izdebug {
+
+class RingBuffer
+{
+public:
+    RingBuffer();
+    RingBuffer(unsigned long size=0);
+    ~RingBuffer();
+
+
+private:
+    unsigned long m_size;
+    void* m_data;
+};
+
+
+} // namespace izdebug
+
+
 #endif // RINGBUFFER_H
