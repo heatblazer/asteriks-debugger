8fcbddb6c5549ea39021814caeecd2963ce02fec
heatblazer <ilianzapryanov@abv.bg>
2016-09-13 23:02:04 +0300

    added more to alsadebugger: test it in gui app

--- alsa-debugger.cpp
+++ alsa-debugger.cpp
@@ -1,6 +1,115 @@
 #include "alsa-debugger.h"
 
+// debug messages //
+#include <iostream>
+
 namespace izdebug {
+
+ALSAAbstract::ALSAAbstract()
+    : p_handle(nullptr),
+      p_params(nullptr)
+{
+
+}
+
+ALSAAbstract::~ALSAAbstract()
+{
+    std::cout << "~ALSAAbstrace" << std::endl;
+}
+
+void ALSAAbstract::cleanup()
+{
+    // cleanup TODO
+}
+
+ALSAPlayer::ALSAPlayer():
+    ALSAAbstract()
+{
+
+}
+
+ALSAPlayer::~ALSAPlayer()
+{
+    std::cout << "~ALSAPlayer" << std::endl;
+    _cleanup();
+}
+
+bool ALSAPlayer::init()
+{
+    return true;
+}
+
+
+
+void ALSAPlayer::_cleanup()
+{
+    // clean stuff then call
+
+    ALSAAbstract::cleanup();
+}
+
+ALSARecorder::ALSARecorder()
+    : ALSAAbstract()
+{
+
+}
+
+ALSARecorder::~ALSARecorder()
+{
+    std::cout << "~ALSARecorder" << std::endl;
+    _cleanup();
+}
+
+bool ALSARecorder::init()
+{
+
+}
+
+void ALSARecorder::_cleanup()
+{
+    // clean your stuff then call
+    ALSAAbstract::cleanup();
+}
+
+ALSADebugger::ALSADebugger()
+    : p_player(nullptr),
+      p_recorder(nullptr)
+{
+
+}
+
+ALSADebugger::~ALSADebugger()
+{
+    if (p_player) {
+        delete p_player;
+    }
+
+    if (p_recorder) {
+        delete p_recorder;
+    }
+}
+
+bool ALSADebugger::create(bool has_player, bool has_rec)
+{
+    bool ret = true;
+
+    if (has_player) {
+        p_player = new ALSAPlayer;
+        if (!p_player) {
+            ret &= false;
+        }
+    }
+
+    if (has_rec) {
+        p_recorder = new ALSARecorder;
+        if(!p_recorder) {
+            ret &= false;
+        }
+    }
+
+    return ret;
+}
+
 
 } // namespace izdebug
 
--- alsa-debugger.h
+++ alsa-debugger.h
@@ -1,30 +1,54 @@
 #ifndef ALSADEBUGGER_H
 #define ALSADEBUGGER_H
 
+#include <alsa/asoundlib.h>
 
 // capture samples from the audio device
 
 namespace izdebug {
+class ALSAAbstract;
 class ALSAPlayer;
 class ALSARecorder;
 class ALSADebugger;
 
-class ALSAPlayer
+
+class ALSAAbstract
+{
+public:
+    ALSAAbstract();
+    virtual ~ALSAAbstract();
+    virtual bool init()=0;
+    virtual void cleanup();
+
+protected:
+    snd_pcm_t*  p_handle;
+    snd_pcm_hw_params_t* p_params;
+};
+
+class ALSAPlayer : public ALSAAbstract
 {
 public:
     ALSAPlayer();
     ~ALSAPlayer();
     bool init();
+
+private:
+    // custom cleanup
+    void _cleanup();
+
+private:
+
 };
 
-class ALSARecorder
+class ALSARecorder : public ALSAAbstract
 {
 public:
     ALSARecorder();
     ~ALSARecorder();
     bool init();
 
-
+private:
+    void _cleanup();
 };
 
 class ALSADebugger
@@ -35,11 +59,9 @@ public:
 
     bool create(bool has_player=true, bool has_rec=true);
 
-
-
 private:
-    ALSAPlayer*   p_player;
-    ALSARecorder* p_recorder;
+    ALSAAbstract*   p_player;
+    ALSAAbstract*   p_recorder;
 };
 
 
